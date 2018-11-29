8b7c5a6bbb2e52fabe4731bf16f8fda2c1c11e8c
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-11-29 23:20:22 +0530

    Update WASenderAccSvc.java

--- app/src/main/java/ga/nikhilkumar/whatsappsender/WASenderAccSvc.java
+++ app/src/main/java/ga/nikhilkumar/whatsappsender/WASenderAccSvc.java
@@ -4,7 +4,10 @@ import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
 import android.preference.PreferenceManager;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Toast;
+
+import java.util.List;
 
 import static android.view.accessibility.AccessibilityNodeInfo.ACTION_CLICK;
 
@@ -18,7 +21,10 @@ public class WASenderAccSvc extends AccessibilityService {
         if (AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED == event.getEventType()) {
             String actname = event.getClassName().toString();
             if (actname.equals("com.whatsapp.Conversation")) {
-                getRootInActiveWindow().findAccessibilityNodeInfosByViewId("com.whatsapp:id/send").get(0).performAction(ACTION_CLICK);
+                List<AccessibilityNodeInfo> nodes = getRootInActiveWindow().findAccessibilityNodeInfosByViewId("com.whatsapp:id/send");
+                if (nodes.size()>0) {
+                    nodes.get(0).performAction(ACTION_CLICK);
+                }
                 performGlobalAction(GLOBAL_ACTION_BACK);
             } else if (actname.equals("com.whatsapp.HomeActivity")) {
                 sendNext();
